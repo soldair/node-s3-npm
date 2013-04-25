bd7177fb3186a524127237a10732c4a847a4ca83
Ryan Day <soldair@gmail.com>
2013-04-25 15:02:13 -0700

    more work on npm json

--- test/publish.js
+++ test/publish.js
@@ -3,11 +3,10 @@ var publish = require('../lib/publish');
 var config = require('../config');
 
 test("test info",function(t){
-  t.plan(1);
   publish(config,function(err,data){
-    cosole.log('PUBLISH CALLED BACK');
-    console.log(err,data);
-
+    var versions = Object.keys(data.versions);
     t.ok(!err,'should not have error publishing');
+    t.ok(data.versions[versions[0]].dist.sha,'must have sha in dist');
+    t.end();
   });
 });

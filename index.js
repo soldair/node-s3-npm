73fc22ac28b5c9f1f90b7d76f86e798df0a5339b
Ryan Day <soldair@gmail.com>
2013-05-03 10:54:14 -0700

    woo publish works!

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ var log = require('./lib/log');
 
 module.exports = function(config){
 
-  log('info','config',config);
+  //log('info','config',config);
 
   var cmd = config._[0];
   var cmds = {
     publish:function(config){
-      publish(config); 
+      publish(config,function(err,data){
+        if(err) return log('error',err);
+        log('success',data);
+      }); 
     },
     info:function(config){
       info(config,config._[1],function(err,data){
@@ -28,7 +31,7 @@ module.exports = function(config){
 
   if(cmds[cmd]) {
 
-    cmds[cmd](config)
+    cmds[cmd](config);
 
   } else {
     log('error','unknown command ',cmd);

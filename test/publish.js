fe55c8e2fce45370cf3d829f3c4162f3a7c4b756
Ryan Day <soldair@gmail.com>
2013-05-03 11:09:52 -0700

    work on publish test

--- test/publish.js
+++ test/publish.js
@@ -2,12 +2,17 @@ var test = require('tap').test;
 var publish = require('../lib/publish');
 var config = require('../config');
 
-test("test info",function(t){
+test("test publish",function(t){
+  config.force = true;
   publish(config,function(err,data){
     console.log(data);
     var versions = Object.keys(data.versions);
+
+
+    console.log(data.versions[versions[0]].dist);
+
     t.ok(!err,'should not have error publishing');
-    t.ok(data.versions[versions[0]].dist.sha,'must have sha in dist');
+    t.ok(data.versions[versions[0]].dist.shasum,'must have sha in dist');
     t.end();
   });
 });

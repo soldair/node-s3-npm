713ea67f3ea3fe39e646422c41db26f6b35c00de
Ryan Day <soldair@gmail.com>
2013-05-03 17:10:17 -0700

    fixing exit code on test and adding debug for secure vars

--- config.js
+++ config.js
@@ -3,13 +3,17 @@ var argv = require('optimist').argv;
 config._ = argv._;
 
 if(process.env.S3NPM_KEY) {
+  console.log('have key');
   config.key = process.env.S3NPM_KEY;
+} else {
+  console.log('no key!!!');
 }
+
 if(process.env.S3NPM_SECRET) {
-  console.log('')
+  console.log('secret!!');
   config.secret = process.env.S3NPM_SECRET;
 } else {
-
+  console.log('no secret!!');
 }
 
 module.exports = config;
